c31c3a9191e3d5e7b669bba2c80a5b76c2ecb910
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-29 16:48:08 +0700

    update worker

--- worker.go
+++ worker.go
@@ -1,1 +1,40 @@
 package gubrak
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// Result struct
+type Result struct {
+	TotalSuccess uint64
+	TotalFail    uint64
+}
+
+// Scan func
+func Scan(jobs chan<- *http.Response,
+	client *Client,
+	method string,
+	path string,
+	payload io.Reader,
+	headers map[string]string,
+	requestSize uint64) {
+	var i uint64
+	for i = 1; i <= requestSize; i++ {
+		response, err := client.Do(method, path, payload, nil, headers)
+		if err != nil {
+			fmt.Println(err)
+		}
+		jobs <- response
+	}
+
+	close(jobs)
+}
+
+// Consume func
+func Consume(id uint64, jobs <-chan *http.Response, results chan<- *http.Response) {
+	for job := range jobs {
+		results <- job
+	}
+}
