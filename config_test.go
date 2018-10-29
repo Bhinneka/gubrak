b2edd18c5a0a602cabf0c67c9a3f01ad6fd46d2d
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-29 13:03:43 +0700

    add config test

---
+++ config_test.go
@@ -0,0 +1,24 @@
+package gubrak
+
+import (
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	config, err := LoadConfig()
+
+	if err != nil {
+		t.Error("load config should not error")
+	}
+
+	expectedBaseURL := "http://example.com"
+	expectedAuthorization := "Basic exnfekeoeoeojsjalaljahhd"
+
+	if config.BaseURL != expectedBaseURL {
+		t.Error("expectedBaseURL should equal config.BaseURL")
+	}
+
+	if config.Headers["Authorization"] != expectedAuthorization {
+		t.Error("expectedAuthorization should equal headers.Authorization")
+	}
+}
