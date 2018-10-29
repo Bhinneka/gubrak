7250b33d6ed1092a01896a90ecc5975340e1e9b2
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-29 17:27:08 +0700

    update error message

--- errors.go
+++ errors.go
@@ -6,8 +6,8 @@ import (
 
 var (
 	// ErrorConfigNotFound for error config file not found
-	ErrorConfigNotFound = errors.New("config file not found")
+	ErrorConfigNotFound = errors.New("--config file not found--")
 
 	// ErrorFormatConfigNotValid for invalid format config error
-	ErrorFormatConfigNotValid = errors.New("format config is not valid")
+	ErrorFormatConfigNotValid = errors.New("--format config is not valid--")
 )
