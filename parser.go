9bdb06df66d722fbe29c04e7be69c53e7e262a31
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-29 16:47:23 +0700

    add arguments parser

---
+++ parser.go
@@ -0,0 +1,53 @@
+package gubrak
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+// Argument struct
+type Argument struct {
+	Method     string
+	URL        string
+	RequestNum uint64
+	Help       func()
+}
+
+// ParseArgument for parse argument to Argument
+func ParseArgument() (*Argument, error) {
+
+	numOfRequest := flag.Uint64("r", DefaultRequestNum, "number of thread")
+	method := flag.String("m", "GET", "http method")
+	urlPath := flag.String("u", "", "full url path")
+
+	flag.Usage = func() {
+		fmt.Println("	-m", "http method")
+		fmt.Println("	-r", "number of request, default = 100")
+		fmt.Println("	-u", "full url path")
+		fmt.Println("	-h or --help for show help")
+		fmt.Println("	and the third argument is the ip address")
+	}
+
+	if len(*method) < 0 {
+		return &Argument{Help: flag.Usage}, errors.New("	(-m) method parameter required")
+	}
+
+	// if len(args) <= 1 {
+	// 	return &Argument{Help: flag.Usage}, errors.New("	not enough arguments")
+	// }
+
+	// arg := args[1:]
+
+	// var ipAddress string
+	// if len(arg) > 2 {
+	// 	ipAddress = arg[2]
+	// } else {
+	// 	ipAddress = arg[0]
+	// }
+
+	flag.Parse()
+
+	return &Argument{Method: strings.ToUpper(*method), URL: *urlPath, RequestNum: *numOfRequest}, nil
+}
