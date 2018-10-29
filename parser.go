78438275ee71044898dc4d77508bf02e052e43b5
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-29 17:27:19 +0700

    move config to parser

--- parser.go
+++ parser.go
@@ -10,6 +10,7 @@ import (
 // Argument struct
 type Argument struct {
 	Method     string
+	Config     string
 	URL        string
 	RequestNum uint64
 	Help       func()
@@ -21,11 +22,13 @@ func ParseArgument() (*Argument, error) {
 	numOfRequest := flag.Uint64("r", DefaultRequestNum, "number of thread")
 	method := flag.String("m", "GET", "http method")
 	urlPath := flag.String("u", "", "full url path")
+	configPath := flag.String("c", "config.json", "full url path")
 
 	flag.Usage = func() {
 		fmt.Println("	-m", "http method")
 		fmt.Println("	-r", "number of request, default = 100")
 		fmt.Println("	-u", "full url path")
+		fmt.Println("	-c", "your config path, default config.json")
 		fmt.Println("	-h or --help for show help")
 		fmt.Println("	and the third argument is the ip address")
 	}
@@ -49,5 +52,5 @@ func ParseArgument() (*Argument, error) {
 
 	flag.Parse()
 
-	return &Argument{Method: strings.ToUpper(*method), URL: *urlPath, RequestNum: *numOfRequest}, nil
+	return &Argument{Method: strings.ToUpper(*method), Config: *configPath, URL: *urlPath, RequestNum: *numOfRequest, Help: flag.Usage}, nil
 }
