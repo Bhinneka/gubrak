db3aed53ee5f16e55e46bd2e49b41752b6bce7e1
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-31 06:56:26 +0700

    add version flag

--- parser.go
+++ parser.go
@@ -4,33 +4,42 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"os"
 	"strings"
 )
 
 // Argument struct
 type Argument struct {
-	Method     string
-	Config     string
-	URL        string
-	RequestNum uint64
-	Help       func()
+	Method      string
+	Config      string
+	URL         string
+	RequestNum  uint64
+	ShowVersion bool
+	Help        func()
 }
 
 // ParseArgument for parse argument to Argument
 func ParseArgument() (*Argument, error) {
+
+	// var (
+	// 	showVersion bool
+	// )
 
 	numOfRequest := flag.Uint64("r", DefaultRequestNum, "number of thread")
 	method := flag.String("m", "GET", "http method")
 	urlPath := flag.String("u", "", "full url path")
 	configPath := flag.String("c", "config.json", "full url path")
 
+	showVersion := flag.Bool("v", false, "show version")
+
 	flag.Usage = func() {
-		fmt.Println("	-m", "http method")
-		fmt.Println("	-r", "number of request, default = 100")
-		fmt.Println("	-u", "full url path")
-		fmt.Println("	-c", "your config path, default config.json")
-		fmt.Println("	-h or --help for show help")
-		fmt.Println("	and the third argument is the ip address")
+
+		fmt.Fprintln(os.Stderr, "	-m", "http method")
+		fmt.Fprintln(os.Stderr, "	-r", "number of request, default = 100")
+		fmt.Fprintln(os.Stderr, "	-u", "full url path")
+		fmt.Fprintln(os.Stderr, "	-c", "your config path, default config.json")
+		fmt.Fprintln(os.Stderr, "	-v", "show gubrak version")
+		fmt.Fprintln(os.Stderr, "	-h or --help for show help")
 	}
 
 	if len(*method) < 0 {
@@ -39,5 +48,5 @@ func ParseArgument() (*Argument, error) {
 
 	flag.Parse()
 
-	return &Argument{Method: strings.ToUpper(*method), Config: *configPath, URL: *urlPath, RequestNum: *numOfRequest, Help: flag.Usage}, nil
+	return &Argument{Method: strings.ToUpper(*method), Config: *configPath, URL: *urlPath, RequestNum: *numOfRequest, ShowVersion: *showVersion, Help: flag.Usage}, nil
 }
