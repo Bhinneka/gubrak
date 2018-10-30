a12c2de5bd84900529845ec2375d937d37819020
wuriyanto <wuriyanto48@yahoo.co.id>
2018-10-31 06:57:36 +0700

    cmd add version flag

--- cmd/gubrak/main.go
+++ cmd/gubrak/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/Bhinneka/gubrak"
 )
@@ -13,9 +14,13 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 		args.Help()
+		os.Exit(0)
 	}
 
-	fmt.Println(args)
+	if args.ShowVersion {
+		fmt.Printf("%s version %s (runtime: %s)\n", os.Args[0], gubrak.Version, runtime.Version())
+		os.Exit(0)
+	}
 
 	g, err := gubrak.New(5, args)
 
